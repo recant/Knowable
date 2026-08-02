70fe8cfdac4cfb973716f5c9486a7d6f3792db80
recant <107957705+recant@users.noreply.github.com>
2026-08-02 16:42:59 -0400

    Add Every App manifest

---
+++ everyapp.config.ts
@@ -0,0 +1,8 @@
+export default {
+  id: "knowable",
+  name: "Knowable",
+  description: "Open-source personalized interactive learning, built as a free alternative to paid course apps.",
+  main: "src/entry.worker.js",
+  resources: {},
+  public: [],
+} as const;
